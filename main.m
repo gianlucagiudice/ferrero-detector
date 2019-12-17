590140f3e9b51f37bd01b536920f14696b935422
Gianluca Giudice <gianlu@pop-os.localdomain>
2019-12-17 03:10:04 +0100

    commit

--- main.m
+++ main.m
@@ -5,7 +5,7 @@ addpath(genpath('functions/'));
 images = readlist('data/images.list');
 
 %% Read and manipulate the image
-path = 'images/original/'+string(images{9});
+path = 'images/original/'+string(images{5});
 scale_factor = 0.2;
 
 %% Image Binarization
@@ -13,6 +13,7 @@ limit_num = 30;
 bws = {};
 tic
 parfor i = 1:limit_num
+    path = 'images/original/'+string(images{i});
     bws{i} =  image_to_edge(path, scale_factor);
 end
 toc
