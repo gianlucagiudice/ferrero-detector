12d14fe35c66bbbebcf967555360f6914a1158d4
Gianluca Giudice <gianlu@pop-os.localdomain>
2019-12-18 14:53:46 +0100

    scaling

--- functions/image_to_edge.m
+++ functions/image_to_edge.m
@@ -3,10 +3,10 @@ function out_edge = image_to_edge(path, scale_factor)
     [~, target_image] = read_and_manipulate(path, scale_factor, @rgb2ycbcr, 3);
     %% Image enhancement
     target_image_filtered = medfilt2(target_image, [7 7]);
-    N = 11;
+    N = 13;
     opt_sigma = ((N-1) / 2) / 2.5;
     F3 = fspecial('gaussian', N , opt_sigma);
-    target_image_filtered  = imfilter(target_image_filtered , F3);
+    target_image_filtered  = imfilter(target_image_filtered , F3, 'replicate');
     %% Find edges 
     out_edge = edge(target_image_filtered, 'Canny');
 end
--- tests/canny_to_binary.m
+++ tests/canny_to_binary.m
@@ -6,13 +6,14 @@ images = readlist('../data/images.list');
 
 %% Find Edges
 scale_factor = 0.3;
-path = '../images/original/'+string(images{19});
+% 9; 15; 21; 23; 24; 
+path = '../images/original/'+string(images{21}); %14; 21
 canny_edge = image_to_edge(path, scale_factor);
 
 %% Find box
 [r, c] = size(canny_edge);
 % 13px is the border size
-out = compute_local_descriptors(canny_edge, 11, 5, @compute_average_color);
+out = compute_local_descriptors(canny_edge, 20, 5, @compute_average_color);
 % Label the image using k-means clustering
 labels = kmeans(out.descriptors, 3);
 img_labels = reshape(labels, out.nt_rows, out.nt_cols);
@@ -24,18 +25,23 @@ for label = 1:length(unique(labels))
     label_on_box(label) = sum(img_labels_out == label .* canny_edge, 'all');
 end
 [~, bkg_label] = min(label_on_box);
+bkg_image = img_labels_out == bkg_label;
 
 %% Analisi componetni connesse ---> prendere area maggiore
-
+figure(2);
+imshow(img_labels_out == bkg_label);
 %% Morfologia matematica
 
 figure(1);
 subplot(2,2,1);imshow(canny_edge);title('Edges');
 subplot(2,2,2);imagesc(img_labels_out), axis image;title('Labels');
-subplot(2,2,3);imshow(img_labels_out == bkg_label);title('Background');
+subplot(2,2,3);imshow(img_labels_out == bkg_label);title('Background'); 
 
+
+%{
+ 
 %% TEST TUTTI
-limit_num = 2;
+limit_num = 40;
 labels = {};
 tic
 parfor i = 1:limit_num
@@ -45,12 +51,12 @@ parfor i = 1:limit_num
     %% Find box
     [r, c] = size(c_e);
     % 13px is the border size
-    out = compute_local_descriptors(c_e, 11, 5, @compute_average_color);
+    out = compute_local_descriptors(c_e, 20, 5, @compute_average_color);
     % Label the image using k-means clustering
     lab = kmeans(out.descriptors, 3);
     img_labels = reshape(lab, out.nt_rows, out.nt_cols);
-    img_labels_out = imresize(img_labels, [r, c], 'nearest'); 
-
+    im = imresize(img_labels, [r, c], 'nearest'); 
+    labels{i} = im;
     
 %{
  %% Find label relative to background
@@ -62,7 +68,6 @@ parfor i = 1:limit_num
      
 %}
 
-    labels{i} = img_labels_out;
 
 end
 toc
@@ -70,5 +75,7 @@ toc
 %% Show results
 for i = 1:limit_num
     figure(i);
-    imshow(labels{i}); 
+    imagesc(labels{i}), axis image;title('Labels');
 end
+ 
+%}
