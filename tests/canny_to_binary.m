e22d4f2acbe4f53d8580d054c3840c18c0cf40f8
Gianluca Giudice <gianlu@pop-os.localdomain>
2019-12-18 20:14:25 +0100

    find box mask

--- tests/canny_to_binary.m
+++ tests/canny_to_binary.m
@@ -27,7 +27,7 @@ end
 [~, bkg_label] = min(label_on_box);
 bkg_image = img_labels_out == bkg_label;
 
-%% Analyze connected components of background to fill box
+%% Analyze connected components of background to fill up box
 bkg_labels = bwlabel(bkg_image);
 % Area of background components
 areas = histogram(bkg_labels(bkg_labels > 0));
@@ -38,11 +38,16 @@ bkg_mask = (bkg_labels ~= real_bkg) .* (bkg_labels > 0);
 real_non_bkg = abs(bkg_image-1) | bkg_mask;
 
 %% Find the box
+objects = bwlabel(real_non_bkg);
+areas = histogram(objects(objects > 0));
+% Real boxlabel 
+[~, box_label] = max(areas.Values);
+% Find backgournd in box
+box_mask = (objects == box_label) .* (objects > 0);
+real_box = real_non_bkg & box_mask;
 
 %% Enhancement
-%bkg_image_filtered = medfilt2(bkg_image, [50 50], 'symmetric');
-
-%% Morfologia matematica
+real_box = medfilt2(real_box, [50 50]);
 
 %{
  se = strel('disk', 50, 6);
@@ -59,8 +64,8 @@ subplot(3,2,1);imshow(canny_edge);title('Edges');
 subplot(3,2,2);imagesc(img_labels_out), axis image; title('Labels');
 subplot(3,2,3);imshow(bkg_image);title('Background'); 
 subplot(3,2,4);imagesc(bkg_labels), axis image;title('Background Labels');
-subplot(3,2,5);imshow(real_non_bkg);title('Background Filtered');
-subplot(3,2,5);imshow(real_box);title('Background Filtered');
+subplot(3,2,5);imshow(real_non_bkg);title('Box filled');
+subplot(3,2,6);imshow(real_box);title('Box');
 
 %{
  
