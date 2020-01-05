61c0cdba467592d6d25c08318aefde6583f82e5b
Gianluca Giudice <gianlu@pop-os.localdomain>
2020-01-05 22:40:31 +0100

    optimization

--- functions/crop_box.m
+++ functions/crop_box.m
@@ -1,4 +1,6 @@
-function [box_cropped, rotated, sheared, M] = crop_box(box_image, vertices, crop_padding)
+function [box_cropped, rotated, sheared, M] = ...
+    crop_box(box_image, vertices, crop_k, corner_k)
+    
     %% Find vectors relative to pivot as origin
     box_edge1 = point_to_vector(vertices(2), vertices(1));
     box_edge2 = point_to_vector(vertices(2), vertices(3));
@@ -16,6 +18,8 @@ function [box_cropped, rotated, sheared, M] = crop_box(box_image, vertices, crop
     origin_v2 =    point_to_vector(origin, vertices(3));
     origin_pivot = point_to_vector(origin, vertices(2));
 
+
+    %% ---------- ROTATION ----------
     %% Rotate longest edge
     R = makeresampler({'cubic','nearest'},'fill');
     if (len1 < len2)
@@ -35,55 +39,38 @@ function [box_cropped, rotated, sheared, M] = crop_box(box_image, vertices, crop
     rotated_o_pivot = (M(1:2, 1:2) * origin_pivot);
     
     %% Evaluate new vertices after rotation using rotated top-left as origin
-    new_v1    = [rotated_o(1) + rotated_o_v1(1);    rotated_o(2) - rotated_o_v1(2)   ];
-    new_v2    = [rotated_o(1) + rotated_o_v2(1);    rotated_o(2) - rotated_o_v2(2)   ];
-    new_pivot = [rotated_o(1) + rotated_o_pivot(1); rotated_o(2) - rotated_o_pivot(2)];
-    rotated.vertices = round([new_v1, new_pivot, new_v2]);
+    new_v1    = round([rotated_o(1) + rotated_o_v1(1);    rotated_o(2) - rotated_o_v1(2)   ]);
+    new_v2    = round([rotated_o(1) + rotated_o_v2(1);    rotated_o(2) - rotated_o_v2(2)   ]);
+    new_pivot = round([rotated_o(1) + rotated_o_pivot(1); rotated_o(2) - rotated_o_pivot(2)]);
+    rotated.vertices = [new_v1, new_pivot, new_v2];
 
     %% Find vectors relative to pivot as origin
     box_edge1 = point_to_vector(new_pivot, new_v1);
     box_edge2 = point_to_vector(new_pivot, new_v2);
     
     if (norm(box_edge1) > norm(box_edge2))
-        longest  = new_v1;
-        shortest = new_v2;
-        longest_pivot = box_edge1;
+        longest_edge = box_edge1;
     else
-        % Posso anche non farli e farli dopo
-        shortest = new_v1;
-        longest  = new_v2;
-
-        longest_pivot = box_edge2;
+        longest_edge = box_edge2;
     end
 
-    %% Shear horizontal longest edge
-    n_longest = longest_pivot / norm(longest_pivot);
-    % M is the negative sine of the longest vector
-    %% Il sengo dipende dal coseno/seno di n_longest (?)
-    
+
+    %% ---------- SHEAR ----------
+    n_longest = longest_edge / norm(longest_edge);
     m = n_longest(2);
-    save_M = M;
     
     %% Pivot is in rigth position relative to other vertices
     if (new_pivot(1) > new_v1(1) || new_pivot(1) > new_v2(1))
        m = -m;
-       flag = 1;
     end
-    %M = [1 0 0; m 1 0; 0 0 1];
     
+    %% Shear image horizontal
     M = [1 m 0; 0 1 0; 0 0 1];
     T = maketform('affine', M);
     sheared.image = imtransform(rotated.image, T, R,'FillValues',0);
-    
+
+    %% Evaluate new vertices after shear
     if (n_longest(2) < 0 )
-        m = m;
-        sheared_o = find_new_origin(rotated.image, save_M);
-        padding = 72;
-        padding = [0; sheared_o(2)];
-        padding = 72;
-        r = rotated.image(:,1,1);
-        g = rotated.image(:,1,2);
-        b = rotated.image(:,1,3);
         left_corner_y_rotated = find(sum(rotated.image(:, 1, :), 3) > 0, 1); 
         left_corner_y_sheared = find(sum(sheared.image(:, 1, :), 3) > 0, 1); 
         y_padding = left_corner_y_sheared - left_corner_y_rotated;
@@ -92,113 +79,32 @@ function [box_cropped, rotated, sheared, M] = crop_box(box_image, vertices, crop
         padding = [0; 0];
     end
     
-
-
     M = [1 0 0; m 1 0 ; 0 0 1];
-    %M = [1 -m 0; 0 1 0 ; 0 0 1];
-    new_v1    = M(1:2, 1:2) * new_v1;
-    new_v2    = M(1:2, 1:2) * new_v2;
-    new_pivot = M(1:2, 1:2) * new_pivot;
+    new_v1    = M(1:2, 1:2) * new_v1    + padding;
+    new_v2    = M(1:2, 1:2) * new_v2    + padding;
+    new_pivot = M(1:2, 1:2) * new_pivot + padding;
+    sheared.vertices = [new_v1, new_pivot, new_v2];
     
-    %padding = [0; 329];
-    new_v1 = new_v1 + padding;
-    new_v2 = new_v2 + padding;
-    new_pivot = new_pivot + padding;
-    
-    
-    
-    figure(7);
-    imshow(sheared.image);
-    plot_vertices(round([new_v1, new_pivot, new_v2]));
-    
-    m;
-    
-%{
- 
-    %% Find vectors before lineare transformation using top-left side as origin
-    origin.pivot = 'origin';
-    origin.value = [1; 1];
-    origin_v1 =    point_to_vector(origin, new_v1);
-    origin_v2 =    point_to_vector(origin, new_v2);
-    origin_pivot = point_to_vector(origin, new_pivot);
 
     if (norm(box_edge1) > norm(box_edge2))
         longest  = new_v1;
         shortest = new_v2;
-        longest_pivot = box_edge1;
+        shortest_edge = box_edge2;
     else
-        % Posso anche non farli e farli dopo
         shortest = new_v1;
         longest  = new_v2;
-
-        longest_pivot = box_edge2;
-    end
-
-
- 
-%}
-
-
-
-    
-%{
- 
-    %% Find new origin after shear
-    sheared_o = find_new_origin(sheared, save_M);
-    
-    %% Find vectors after rotation
-    shear_v1    = (M(1:2, 1:2) * origin_v1);
-    shear_v2    = (M(1:2, 1:2) * origin_v2);
-    shear_pivot = (M(1:2, 1:2) * origin_pivot);
-    
-    %% Evaluate new vertices after shear using top-left as origin
-    new_v1    = [sheared_o(1) + shear_v1(1);    sheared_o(2) - shear_v1(2)   ];
-    new_v2    = [sheared_o(1) + shear_v2(1);    sheared_o(2) - shear_v2(2)   ];
-    new_pivot = [sheared_o(1) + shear_pivot(1); sheared_o(2) - shear_pivot(2)];
-    
-    
-    %% Find vectors relative to pivot as origin
-    box_edge1 = point_to_vector(new_pivot, new_v1);
-    box_edge2 = point_to_vector(new_pivot, new_v2);
-    
-
-
-    %% ---------- Shear ----------
-    %%% Find new origin after rotation
-    %[n_rows_rotated, n_cols_rotated] = size(rotated);
-    
-    %% Non sono sicuro di questa cosa
-    sheared_o = rotated_o;
-    sheared_o = find_new_origin(sheared, save_M);
-    
-    %% Find vectors after rotation
-    shear_v1    = M(1:2, 1:2) * rotated_o_v1;
-    shear_v2    = M(1:2, 1:2) * rotated_o_v2;
-    shear_pivot = M(1:2, 1:2) * rotated_o_pivot;
-    
-    %% Evaluate new vertices after shear using top-left as origin
-    new_v1    = [sheared_o(1) + shear_v1(1);    sheared_o(2) - shear_v1(2)   ];
-    new_v2    = [sheared_o(1) + shear_v2(1);    sheared_o(2) - shear_v2(2)   ];
-    new_pivot = [sheared_o(1) + shear_pivot(1); sheared_o(2) - shear_pivot(2)];
-    
-    %% Find vectors relative to pivot as origin
-    box_edge1 = point_to_vector(new_pivot, new_v1);
-    box_edge2 = point_to_vector(new_pivot, new_v2); 
-%}
-
-    
-    if (norm(box_edge1) > norm(box_edge2))
-        longest  = new_v1;
-        shortest = new_v2;
-    else
-        % Posso anche non farli e farli dopo
-        shortest = new_v1;
-        longest  = new_v2;
+        shortest_edge = box_edge1;
     end
      
-
     
-    %% Consider all cases to figure out the orientation
+    %% Evaluate padding    
+    crop_padding = abs(longest_edge(2)) * crop_k;
+    corner_padding = round(norm(shortest_edge) * corner_k);
+    
+    disp(crop_padding);
+    disp(corner_padding);
+    
+    %% Consider all cases to figure out the Pivot orientation
     if(     (box_edge1(2) == 0 && box_edge1(1) > 0 && box_edge2(2) < 0) || ...
             (box_edge2(1) == 0 && box_edge2(2) < 0 && box_edge1(2) > 0) || ...
             (box_edge2(2) == 0 && box_edge2(1) > 0 && box_edge1(2) < 0) || ...
@@ -208,6 +114,7 @@ function [box_cropped, rotated, sheared, M] = crop_box(box_image, vertices, crop
         rectangle_y = new_pivot(2);
         rectangle_width  = new_pivot(1) - longest(1);
         rectangle_height = new_pivot(2) - shortest(2);
+        disp("qui");
 
     elseif( (box_edge2(2) == 0 && box_edge2(1) < 0 && box_edge1(2) < 0) || ...
             (box_edge1(1) == 0 && box_edge1(2) < 0 && box_edge2(1) < 0) || ...
@@ -218,6 +125,7 @@ function [box_cropped, rotated, sheared, M] = crop_box(box_image, vertices, crop
         rectangle_y = longest(2);
         rectangle_width  = new_pivot(1) - longest(1);
         rectangle_height = longest(2) - shortest(2);
+        disp("quo");
 
     elseif( (box_edge1(1) == 0 && box_edge1(2) > 0 && box_edge2(1) > 0) || ...
             (box_edge2(2) == 0 && box_edge2(1) > 0 && box_edge1(2) > 0) || ...
@@ -228,9 +136,18 @@ function [box_cropped, rotated, sheared, M] = crop_box(box_image, vertices, crop
         rectangle_y = shortest(2);
         rectangle_width  = longest(1) - new_pivot(1);
         rectangle_height = new_pivot(2) - shortest(2);
+        
+        % Add padding
+        rectangle_x      = rectangle_x                     + corner_padding; 
+        rectangle_y      = rectangle_y      - crop_padding;
+        rectangle_width  = rectangle_width  + crop_padding;
+        rectangle_height = rectangle_height + crop_padding - corner_padding;
+
+
+        disp("qua");
 
     elseif( (box_edge1(2) == 0 && box_edge1(1) < 0 && box_edge2(2) > 0) || ...
-            (box_edge2(1) == 0 && box_edge2(2) > 0 && box_ede1(1) < 0)  || ...
+            (box_edge2(1) == 0 && box_edge2(2) > 0 && box_ede1(1)  < 0) || ...
             (box_edge2(2) == 0 && box_edge2(1) < 0 && box_edge1(2) > 0) || ...
             (box_edge1(1) == 0 && box_edge1(2) > 0 && box_edge2(1) < 0))
         %% SE
@@ -238,32 +155,16 @@ function [box_cropped, rotated, sheared, M] = crop_box(box_image, vertices, crop
         rectangle_height = new_pivot(2) - shortest(2);
         rectangle_x = new_pivot(1) - rectangle_width;
         rectangle_y = longest(2) - rectangle_height;
-        
-        %{
         % Add padding
-        padding_horizontal = rectangle_width * crop_padding * 0.5;
-        padding_vertical = rectangle_height * crop_padding * 0.5;
-        rectangle_x = rectangle_x - padding_horizontal;
-        rectangle_y = rectangle_y - padding_vertical;
-        rectangle_width  = rectangle_width  + padding_horizontal;
-        rectangle_height = rectangle_height + padding_vertical;  
-        %}
+        rectangle_x      = rectangle_x      - crop_padding;
+        rectangle_y      = rectangle_y      - crop_padding;
+        rectangle_width  = rectangle_width  + crop_padding - corner_padding;
+        rectangle_height = rectangle_height + crop_padding - corner_padding;
+
+        disp("que");
 
 
     end
-
-    %% Add padding
-
-    %{
-    padding_horizontal = rectangle_width * crop_padding * 0.5;
-    padding_vertical = rectangle_height * crop_padding * 0.5;
-    
-    rectangle_x = rectangle_x - padding_horizontal;
-    rectangle_y = rectangle_y - padding_vertical;
-    rectangle_width  = rectangle_width  + padding_horizontal;
-    rectangle_height = rectangle_height + padding_vertical;  
-    %}
-
 
     %% Crop box
     crop_rec = [rectangle_x, rectangle_y, rectangle_width, rectangle_height];
