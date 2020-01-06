1668047188f603f52fe6c28d66582123ee6886da
Gianluca Giudice <gianlu@pop-os.localdomain>
2020-01-06 14:59:58 +0100

    rimosso scatole con edge detection non valido

--- tests/edge_ratio_test.m
+++ tests/edge_ratio_test.m
@@ -19,13 +19,19 @@ for i = 1:length(images_list)
     vertices45 = find_vertices_45(bw);
     best_vertices = decide_best_vertices(vertices45, vertices90);
 
-    %{
-    sf = 0.10; 
-    figure(i);
+    figure(1);
+
+
+%{
+     sf = 0.10; 
     imshow(imresize(scaled_image, sf));title("Best vertices method");
     plot_vertices(round([best_vertices.value] .* sf));
-    %}
+ 
+%}
 
     imshow(scaled_image);title("Best vertices method");
+    plot_vertices(best_vertices);
+    disp(i);
+
 end
 toc
