fd75b83409cad1ebb7cacbcfd65f170a14b90724
Gianluca Giudice <gianlu.giudice@gmail.com>
2020-02-02 18:19:06 +0100

    find mask table

--- functions/find_table_cropped.m
+++ functions/find_table_cropped.m
@@ -1,13 +1,15 @@
-function table_mask_close = find_table_cropped(box_cropped)
+function table_mask_cropped = find_table_cropped(box_cropped)
     %% Edge detection
     box_cropped_ycbcr = rgb2ycbcr(box_cropped);
     box_cropped_edge = edge(box_cropped_ycbcr(:,:,2), 'canny');
     
-    %% Box size
+    %% Compute sum of window T
+    padding = 150;
+    half_padding = round(padding / 2);
+    box_cropped_edge = ...
+    padarray(box_cropped_edge, [padding padding], 0, 'both');
     [r, c] = size(box_cropped_edge);
     
-    %% Compute sum of window T
-    box_cropped_edge = padarray(box_cropped_edge, [50 50], 0, 'both');
     out = compute_local_descriptors(box_cropped_edge, 10, 3, @compute_sum);
     values = out.descriptors;
     
@@ -16,14 +18,7 @@ function table_mask_close = find_table_cropped(box_cropped)
     img_labels = reshape(labels, out.nt_rows, out.nt_cols);
     img_labels_out = imresize(img_labels, [r, c], 'nearest'); 
     edge_mask = abs(1 - img_labels_out);
-    
-    %% Close holes in edge mask
-    se = strel('line', 20, 0);
-    edge_mask_open = imopen(edge_mask, se);
-    se = strel('line', 20, 90);
-    edge_mask_open = imopen(edge_mask_open, se);
-    
-    edge_mask_open = imopen(edge_mask, strel('square', 60));
+    edge_mask_open = imopen(edge_mask, strel('square', 15));
 
     %% Label all edge component
     edge_mask_labeled = bwlabel(edge_mask_open, 8);
@@ -52,61 +47,67 @@ function table_mask_close = find_table_cropped(box_cropped)
         table_mask = table_mask | label_mask;
     end
 
-
-    %% Close holes table mask
-    se = strel('square', 50);
-    table_mask_close = imclose(table_mask, se);
-    
-
-    padding = 150;
-    D = padarray(table_mask,[padding padding],1,'both');
-    
-    D = medfilt2(D, [80 80]);
-
-    se = strel('square', 150);
-    D = imclose(D, se);
-
-    se = strel('square', 40);
-    D = imerode(D, se);
-    
-    half_padding = round(padding /2);
-    X = imcrop(D, [padding, padding, c-1, r-1]);
-
-    A = repmat(abs(1-X), 1, 1, 3) .* box_cropped;
-
-    table_mask_close = A;
-
+    %% Box shape enhancement
 %{
  
-    %% Show results
-    figure(5);
-    
-    subplot(2,4,1); imshow(box_cropped);
-    title("Box cropped");
-
-    subplot(2,4,2); imshow(box_cropped_edge);
-    title("Box cropped edge");
-    
-    subplot(2,4,3); imagesc(img_labels_out), axis image;
-    title("Edge binary labels");
-    
-    subplot(2,4,4); imshow(edge_mask_open);
-    title("Edge mask imopen()");
-    
-    subplot(2,4,5); imagesc(edge_mask_labeled), axis image;
-    title("Edge mask labels");
-    
-    subplot(2,4,6); imshow(table_mask);
-    title("Table mask");
- 
-    subplot(2,4,7); imshow(A);
-    title("Table mask imdilate()");
-
-
-    subplot(2,4,8); imshow(table_mask_close);
-    title("Table mask imclose()");
+    mask_padding = 150;
+    half_padding = round(mask_padding/2);
+    % Add padding
+    table_mask_padding = ...
+    padarray(table_mask, [mask_padding mask_padding], 1, 'both');
  
 %}
 
+    % Adjust shape
+    se = strel('square', padding);
+    table_mask_close = imclose(table_mask, se);
+    table_mask_open = imopen(table_mask_close, se);
+    table_mask_filtered = medfilt2(table_mask_open, [half_padding half_padding]);
+
+    %% Apply mask to box cropped
+    %total_padding = padding + mask_padding;
+
+    [r, c, ~] = size(box_cropped);
+    rectangle_crop = [padding, padding, c-1, r-1];
+    table_mask_cropped = imcrop(table_mask_filtered, rectangle_crop);
+    mask_apllied = box_cropped .* abs(1-table_mask_cropped);
+
+    %{
+    %% Show results
+    figure(5);
+    
+    subplot(2,5,1); imshow(box_cropped);
+    title("Box cropped");
+
+    subplot(2,5,2); imshow(box_cropped_edge);
+    title("Box cropped edge");
+    
+    subplot(2,5,3); imagesc(img_labels_out), axis image;
+    title("Edge binary labels");
+    
+    subplot(2,5,4); imshow(edge_mask_open);
+    title("Edge mask imopen()");
+    
+    subplot(2,5,5); imagesc(edge_mask_labeled), axis image;
+    title("Edge mask labels");
+    
+    subplot(2,5,6); imshow(table_mask);
+    title("Table mask");
+ 
+    subplot(2,5,7); imshow(table_mask_close);
+    title("Table mask imclose()");
+
+    subplot(2,5,8); imshow(table_mask_open);
+    title("Table mask imopen()");
+
+    subplot(2,5,9); imshow(table_mask_filtered);
+    title("Table mask medfilt2()");
+
+    subplot(2,5,10); imshow(mask_apllied);
+    title("Box cropped masked");
+ 
+    %}
+
+    table_mask_cropped = mask_apllied;
 
 end
--- tests/classify_table_test.m
+++ tests/classify_table_test.m
@@ -5,12 +5,13 @@ images_list = readlist('../data/images.list');
 scale_factor = 0.5;
 crop_padding = 0.10;
 
-start_index = 1;
+%% Test particolari: 16, 
+start_index = 18;
 end_index = numel(images_list);
-%end_index = start_index;
+end_index = start_index;
 
 %% Read image
-parfor i = start_index : end_index
+for i = start_index : end_index
     img_path = '../images/original/'+string(images_list{i});
     [~, scaled_image, target_image] = ...
     read_and_manipulate(img_path, scale_factor, @rgb2ycbcr, 3);
@@ -31,13 +32,6 @@ parfor i = start_index : end_index
     %% Classify table
     table_mask = find_table_cropped(box_cropped);
   
-
-%{
-     figure(1);
-    subplot(1,1,1);imshow(table_binary); 
-     
-%}
-
     path = "../images/box_only/mask_" + string(images_list{i});
     imwrite(table_mask, path);
     disp(i);
