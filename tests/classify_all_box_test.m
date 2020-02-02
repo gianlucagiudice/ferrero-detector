5b716eb6a9c991461e39b570d2ae5f7128a51034
Gianluca Giudice <gianlu.giudice@gmail.com>
2020-02-02 22:35:42 +0100

    classification

---
+++ tests/classify_all_box_test.m
@@ -0,0 +1,144 @@
+addpath(genpath('../functions/'));
+load(fullfile('..', 'classifier_bayes.mat'));
+change_color_space = @rgb2hsv;
+
+%% Get list of images
+images_list = readlist('../data/images.list');
+scale_factor = 0.5;
+crop_padding = 0.10;
+
+%% Test particolari: 16, 
+start_index = 1;
+end_index = numel(images_list);
+%end_index = start_index;
+
+%% Read image
+parfor i = start_index : end_index
+        
+    %% Read image
+    % Casi particolari: 6, 8
+    img_path = '../images/original/'+string(images_list{i});
+    [~, scaled_image, target_image] = ...
+    read_and_manipulate(img_path, scale_factor, @rgb2ycbcr, 3);
+
+    %% Box edge
+    canny_edge = image_to_edge(target_image);
+    bw = canny2binary(canny_edge);
+
+    %% Evaluate vertices
+    vertices45 = find_vertices_45(bw);
+    vertices90 = find_vertices_90(bw);
+    best_vertices = decide_best_vertices(vertices45, vertices90);
+
+    %% Cop box
+    [box_cropped, rotated, sheared, rot_matrix] = ...
+    crop_box(scaled_image, best_vertices, crop_padding);
+
+    %% Classification
+    classify_target = change_color_space(box_cropped); 
+    [r, c, ch] = size(classify_target);
+
+    %% Classify choccolates
+    pixs = reshape(classify_target, r*c, 3);
+    predicted = predict(classifier_bayes, pixs);
+    predicted = reshape(predicted, r, c, 1);
+
+    %% Classification enhancement
+    predicted_filtered = medfilt2(predicted, [15 15]);
+    table_mask = find_table_mask_cropped(box_cropped);
+    prediction = predicted_filtered .* abs(1 - table_mask);
+
+    %% Morphologic operator 
+    % Consider the size of a choccolate, btained from dataset
+    choccolate_size_percentage = 0.166;
+    % Evaluate frame size
+
+    %% Lables = [0 = table; 1 = raffaello; 2 = rocher; 3 = rondnoir]
+    %% NOTA BENE: Uso elemento strutturante quadrato perchè la sliding windows sarà quadrata
+
+    %% Adjust raffaello
+    raffaello = prediction == 1;
+    % Close small holes
+    choccolate_fraction = 1/3;
+    tsize = round(r * choccolate_size_percentage * choccolate_fraction);
+    se = strel('disk', tsize);
+    raffaello_mask_closed = imclose(raffaello, se);
+    % Erase non-raffaello
+    choccolate_fraction = 1/4;
+    tsize = round(r * choccolate_size_percentage * choccolate_fraction);
+    se = strel('disk', tsize);
+    raffaello_mask_opened = imopen(raffaello_mask_closed, se);
+    % Dilate raffaello
+    %{
+    choccolate_fraction = 1/4;
+    tsize = round(r * choccolate_size_percentage * choccolate_fraction);
+    se = strel('square', tsize);
+    raffaello_mask_dilated = imdilate(raffaello_mask_opened, se); 
+    %}
+    choccolate_fraction = 1/5;
+    tsize = round(r * choccolate_size_percentage * choccolate_fraction);
+    se = strel('disk', tsize);
+    raffaello_mask_dilated = imdilate(raffaello_mask_opened, se); 
+
+
+
+    %% Adjust rondnoir
+    rondnoir = prediction == 3;
+    % Close small holes
+    choccolate_fraction = 1/5;
+    tsize = round(r * choccolate_size_percentage * choccolate_fraction);
+    se = strel('disk', tsize);
+    rondnoir_mask_closed = imclose(rondnoir, se);
+    % Erase non-rondnoir
+    choccolate_fraction = 1/3;
+    tsize = round(r * choccolate_size_percentage * choccolate_fraction);
+    se = strel('disk', tsize);
+    rondnoir_mask_opened = imopen(rondnoir_mask_closed, se);
+    % Dilate rondnoir
+    choccolate_fraction = 1/4;
+    tsize = round(r * choccolate_size_percentage * choccolate_fraction);
+    se = strel('square', tsize);
+    rondnoir_mask_dilated = imdilate(rondnoir_mask_closed, se);
+
+    %% Evaluate box enhanced
+    box_enhanced = zeros(r, c) + 2;
+    box_enhanced(raffaello_mask_dilated) = 1;
+    box_enhanced(rondnoir_mask_opened) = 3;
+    box_enhanced(table_mask) = 0;
+
+    %% -------- Show results -------- 
+    f = figure('visible', 'off');
+    %% Classification
+    subplot(3,4,1);
+    imshow(box_cropped);title("Box cropped"); 
+    subplot(3,4,2);
+    imagesc(predicted), axis image; title("Labels HSV_S");
+    subplot(3,4,3);
+    imagesc(predicted_filtered), axis image; title("Labels filtered");
+    subplot(3,4,4);
+    imagesc(prediction), axis image; title("Labels with table");
+    %% Raffaello
+    subplot(3,4,5);
+    imshow(raffaello), axis image; title("Raffaello mask");
+    subplot(3,4,6);
+    imshow(raffaello_mask_closed); title("Raffaello mask imclose()");
+    subplot(3,4,7);
+    imshow(raffaello_mask_opened); title("Raffaello mask imopen()");
+    subplot(3,4,8);
+    imshow(raffaello_mask_dilated); title("Raffaello mask imdilate()");
+    %% Rondnoir
+    subplot(3,4,9);
+    imshow(rondnoir), axis image; title("Rondnoir mask");
+    subplot(3,4,10);
+    imshow(rondnoir_mask_closed); title("Rondnoir mask imclose()");
+    subplot(3,4,11);
+    imshow(rondnoir_mask_opened); title("Rondnoir mask imopen()");
+    %% Box enhanced
+    subplot(3,4,12);
+    imagesc(box_enhanced), axis image; title("Box enhanced");
+
+    name = split(string(images_list{i}), '.');
+    path = "../images/classify_box/classified_" + name(1);
+    saveas(f, path, 'png');
+    disp(i);
+end
