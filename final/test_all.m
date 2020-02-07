79e2d0e34d0f7e480e796532312eef053a7ae0f5
Gianluca Giudice <gianlu.giudice@gmail.com>
2020-02-07 01:55:30 +0100

    Images cropped

--- final/test_all.m
+++ final/test_all.m
@@ -6,58 +6,42 @@ boxTypeClassifier = load("classifier/boxTypeClassifier.mat").boxTypeClassifier;
 relPath = "../";
 images = readlist('../data/images.list');
 scaleFactor = 0.5;
-imgPadding = 300;
-
-%% Read images
-disp('Start reading images . . .');
-N = length(images);
-
-parfor targetIndex = 1:N
-    % Read image
-    imgPath = relPath + 'images/original/'+string(images{targetIndex});
-    [originalImage, ~, targetImage] = ...
-        read_and_manipulate(imgPath, scaleFactor, @rgb2ycbcr, 2);
-    % Save
-    imgs{targetIndex}.targetImage = targetImage;
-    imgs{targetIndex}.originalImage = originalImage;
-
-    disp("Read "+targetIndex + " - "+N);
-
-end
-
-disp("Read completed.");
-disp(" - - - - - - -");
+paddingSize = 300;
 
 %% Start processing
+tic
 disp('Start processing . . .');
 
+N = numel(images);
 parfor targetIndex = 1:N
-    % Get a target image
-    targetImage = imgs{targetIndex}.targetImage;
+    imgPath = relPath + 'images/original/'+string(images{targetIndex});
+    [originalImage, scaledImage, targetImage] = ...
+        read_and_manipulate(imgPath, scaleFactor, @rgb2ycbcr, 2);
 
     % Find edges
     cannyEdge = image_to_edge(targetImage);
 
     % I vertici vanno cercati su questa immagine
-    boxMask = box_detection(cannyEdge, imgPadding);
+    boxMask = box_detection(cannyEdge, paddingSize);
 
     % Find vertices
-    vertices = box_vertices(boxMask, imgPadding);
+    vertices = box_vertices(boxMask, paddingSize);
 
     % Classify box type
     typeFeature = compute_type_feature(vertices);
     boxType = boxTypeClassifier.predict(typeFeature);
 
     % Crop box from original image
-    cropTarget = imgs{targetIndex}.originalImage;
+    cropTarget = originalImage;
     sf = length(cropTarget) ./ length(scaledImage);
     cropped = crop_box_perspective(cropTarget, sf, paddingSize, vertices, boxType);
 
     %% Save results
     name = split(string(images{targetIndex}), '.');
-    path = "../images/cropped/cropped_" + name(1);
+    path = "../images/cropped/cropped_"+name(1);
     imwrite(cropped, path + ".png");
 
-    disp("Processed "+targetIndex + " - "+N);
+    disp("Processed "+targetIndex + "-" + N);
 
 end
+toc
